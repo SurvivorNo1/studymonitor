d3655aa689a45550c7e4c1d43453b7029d935f30
LiuYunJie <liuy@my.swjtu.edu.cn>
2024-06-02 02:10:50 +0800

    add workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
+// 使用环境变量来设置基路径，如果没有设置环境变量则默认为 '/'
+const base =  '/'
+
 export default defineConfig({
+  base: base,
   plugins: [react()],
 })
