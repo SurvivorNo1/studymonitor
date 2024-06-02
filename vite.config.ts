380eea907141754dc91b2f23bbc4bd06fceead2e
LiuYunJie <liuy@my.swjtu.edu.cn>
2024-06-02 16:26:13 +0800

    change base

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // 使用环境变量来设置基路径，如果没有设置环境变量则默认为 '/'
-const base = process.env.BASE_URL || '/'
+const base = '/studymonitor/'
 
 export default defineConfig({
   base: base,
